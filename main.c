dba29c1105bb66c3e4b484eaedecc1e0262c9468
Deba1232 <debanjan.das90@gmail.com>
2025-01-20 20:51:57 +0530

    make operand validation proper

--- main.c
+++ main.c
@@ -6,28 +6,49 @@ int main(int argc, char* argv[])
     Dlist *head2=NULL, *tail2=NULL; 
     Dlist *headR=NULL, *tailR=NULL;
 
+	int operand_flag = 1;
 	/* Flag to indicate if the result is negative or zero*/
-	int flag = 0;
+	int sign_flag = 0;
 
 	if(argc == 4){
+		//validation for both the operands
+		char *operand1 = argv[1], *operand2 = argv[3];
 
-		if(isdigit(**(argv + 1)) && isdigit(**(argv + 3))){
+		while(*operand1){
+			if(!isdigit(*operand1)){
+				operand_flag = 0;
+				break;
+			}
+			operand1++;
+		}
+
+		while(*operand2){
+			if(!isdigit(*operand2)){
+				operand_flag = 0;
+				break;
+			}
+			operand2++;
+		}
+
+		if(operand_flag){
 			char operator = **(argv + 2);
 
 			if(operator=='+' || operator=='-' || operator=='x' || operator=='/'){
 				switch (operator){
 					case '+':
-                		operand_to_list(&head1,&tail1,&head2,&tail2,argv,flag);
+                		operand_to_list(&head1,&tail1,&head2,&tail2,argv,sign_flag);
 						/* call the function to perform the addition operation */
-                		addition(&head1,&tail1,&head2,&tail2,&headR,&tailR);
+                		addition(&head1,&tail1,&head2,&tail2,&headR,&tailR, 0);
 						break;
 					case '-':
-						flag = operand_to_list(&head1,&tail1,&head2,&tail2,argv,flag);	
+						sign_flag = operand_to_list(&head1,&tail1,&head2,&tail2,argv,sign_flag);	
 						/* call the function to perform the subtraction operation */
-						subtraction(&head1,&tail1,&head2,&tail2,&headR,&tailR,flag);
+						subtraction(&head1,&tail1,&head2,&tail2,&headR,&tailR,sign_flag);
 						break;
-					case 'x':	
+					case 'x':
+						operand_to_list(&head1,&tail1,&head2,&tail2,argv,sign_flag);	
 						/* call the function to perform the multiplication operation */
+						multiplication(&head1,&tail1,&head2,&tail2,&headR,&tailR);
 						break;
 					case '/':	
 						/* call the function to perform the division operation */
